b3ba872c9238d479f0296531a39085c06bbefb97
choyj0920 <choyj0920@naver.com>
2019-11-27 14:43:43 +0900

    시간 측정

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import LRU, FIFO, LFU, RND 
+from datetime import datetime
 
 def main():
     while(True):
         input_data = list(map(int, input("\n값 입력 : ").split()))
         slotcount = int(input("slotcount : "))
 
+        starttime =datetime.now()
         LRU.LRU_implement(input_data,slotcount)
-        print("\nFIFO : ")
+        print("LRU 수행 시간 : ", (datetime.now()-starttime).microseconds)
+
+        starttime = datetime.now()
         FIFO.FIFO_implement(input_data,slotcount)
+        print("FIFO 수행 시간 : ", (datetime.now()-starttime).microseconds)
+
+        starttime = datetime.now()
         LFU.LFU_implement(input_data,slotcount)
+        print("LFU 수행 시간 : ", (datetime.now()-starttime).microseconds)
+
+        starttime = datetime.now()
         RND.Random_implement(input_data,slotcount)
+        print("RND 수행 시간 : ", (datetime.now()-starttime).microseconds)
+
 
 if __name__ == "__main__":
 	main()        
